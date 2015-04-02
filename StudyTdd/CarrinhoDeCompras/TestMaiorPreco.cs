027cc33909560892170fbe702b5ca0b788f9b85a
David Rodrigues <davidnpr@hotmail.com>
2015-04-01 21:11:44 -0300

    Refatoracao do teste de carrinho de compras

--- StudyTdd/CarrinhoDeCompras/TestMaiorPreco.cs
+++ StudyTdd/CarrinhoDeCompras/TestMaiorPreco.cs
@@ -5,18 +5,23 @@ namespace StudyTdd.CarrinhoDeCompras
     [TestFixture]
     public class TestMaiorPreco
     {
+        private CarrinhoDeCompras carrinho;
+
+        [SetUp]
+        public void Inicialize()
+        {
+            this.carrinho = new CarrinhoDeCompras();
+        }
+
         [Test]
         public void DeveRetornaVazioSeCarrinhoVazio()
         {
-            CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
-
             Assert.AreEqual(0.0, carrinho.MaiorValor(), 0.00001);
         }
 
         [Test]
         public void DeveRetornarValorDoItemSeCarrinhoCom1Elemento()
         {
-            CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
             carrinho.Adiciona(new Item("Fogão", 1, 500.0));
 
             Assert.AreEqual(500.0, carrinho.MaiorValor(), 0.00001);
@@ -25,7 +30,6 @@ namespace StudyTdd.CarrinhoDeCompras
         [Test]
         public void DeveRetornarMaiorValorSeCarrinhoContemMuitosElementos()
         {
-            CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
             carrinho.Adiciona(new Item("Fogão", 1, 500.0));
             carrinho.Adiciona(new Item("Geladeira", 1, 1500.0));
             carrinho.Adiciona(new Item("Tv Led", 1, 750.0));
